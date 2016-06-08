894b43000415f6c04953fe0fd7d690f09521fb97
Esther Goldstein <esthergoldstein@Esthers-MacBook-Pro-2.local>
2016-06-07 20:58:50 -0700

    Done.

--- javacs-lab11/src/com/flatironschool/javacs/WikiCrawler.java
+++ javacs-lab11/src/com/flatironschool/javacs/WikiCrawler.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Queue;
-
+import org.jsoup.nodes.Node;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -53,20 +53,49 @@ public class WikiCrawler {
 	 * @return Number of pages indexed.
 	 * @throws IOException
 	 */
-	public String crawl(boolean testing) throws IOException {
-        // FILL THIS IN!
-		return null;
-	}
-	
-	/**
-	 * Parses paragraphs and adds internal links to the queue.
-	 * 
-	 * @param paragraphs
-	 */
-	// NOTE: absence of access level modifier means package-level
-	void queueInternalLinks(Elements paragraphs) {
-        // FILL THIS IN!
-	}
+	   public String crawl(boolean testing) throws IOException {
+        // filled in
+                String url=queue.remove();
+		System.out.println("URL is: "+url);
+                Elements paragraphs=null;
+                if(testing){
+                        paragraphs=wf.readWikipedia(url);
+                }
+                else{
+                        if(index.isIndexed(url))
+                                return null;
+                        paragraphs=wf.fetchWikipedia(url);
+                }
+                index.indexPage(url, paragraphs);
+                queueInternalLinks(paragraphs);
+                return url;
+        }
+
+        /**
+         * Parses paragraphs and adds internal links to the queue.
+         *
+         * @param paragraphs
+         */
+        // NOTE: absence of access level modifier means package-level
+        void queueInternalLinks(Elements paragraphs) {
+        // filled in
+                String url="";
+                for(Element e : paragraphs){
+                        Iterable<Node> iter = new WikiNodeIterable(e);
+                        for (Node node: iter) {
+                                if (node instanceof Element) {
+                                        Elements links=((Element) node).select("a");
+                                        for(Element f : links) {
+                                                url=((Element) node).attr("href");
+                                                if(url.startsWith("/wiki/"))
+                                                        queue.add("https://en.wikipedia.org"+url);
+                                        }
+                                }
+                        }
+                }
+                System.out.println("size: "+queue.size());
+
+        }
 
 	public static void main(String[] args) throws IOException {
 		
@@ -85,8 +114,6 @@ public class WikiCrawler {
 		do {
 			res = wc.crawl(false);
 
-            // REMOVE THIS BREAK STATEMENT WHEN crawl() IS WORKING
-            break;
 		} while (res == null);
 		
 		Map<String, Integer> map = index.getCounts("the");
